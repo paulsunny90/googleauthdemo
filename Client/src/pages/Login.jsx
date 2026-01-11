087d78a788eee59db3fbefdc279748bf08543f8c
paulsunny90 <paulsunny87267@gmail.com>
2026-01-11 22:35:38 +0530

    login  careted

---
+++ Client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h2>Login</h2>
+      <button onClick={googleLogin}>Login with Google</button>
+    </div>
+  )
+}
+
+export default Login
