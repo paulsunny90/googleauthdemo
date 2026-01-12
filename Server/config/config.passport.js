53e6eab3c2926df8a49f5654948d562fce6269b0
paulsunny90 <paulsunny87267@gmail.com>
2026-01-12 15:00:18 +0530

    server  careted

--- Server/config/config.passport.js
+++ Server/config/config.passport.js
@@ -10,7 +10,7 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGEL_CLIENT_SCRIPT,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:`http://localhost:3000/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
